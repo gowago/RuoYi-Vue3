8954b85425916ea9114c3c8b33170f6eab051916
caiJinCheng <1052690409@qq.com>
2022-10-19 14:38:41 +0800

    1

--- vite/plugins/auto-import.js
+++ vite/plugins/auto-import.js
@@ -1,12 +1,16 @@
-import autoImport from 'unplugin-auto-import/vite'
+import autoImport from "unplugin-auto-import/vite";
 
 export default function createAutoImport() {
-    return autoImport({
-        imports: [
-            'vue',
-            'vue-router',
-            'pinia'
-        ],
-        dts: false
-    })
+  return autoImport({
+    imports: [
+      "vue",
+      "vue-router",
+      "pinia",
+      {
+        "@/hook/hook": ["useInstance"]
+      }
+    ],
+    dts: "./auto-imports.d.ts",
+    dirs: ["/src/hook"]
+  });
 }
--- vite/plugins/svg-icon.js
+++ vite/plugins/svg-icon.js
@@ -1,10 +1,12 @@
-import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-import path from 'path'
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path from "path";
 
 export default function createSvgIcon(isBuild) {
-    return createSvgIconsPlugin({
-		iconDirs: [path.resolve(process.cwd(), 'src/assets/icons/svg')],
-        symbolId: 'icon-[dir]-[name]',
-        svgoOptions: isBuild
-    })
+  return createSvgIconsPlugin({
+    // Specify the icon folder to be cached
+    iconDirs: [path.resolve(process.cwd(), "src/assets/icons/svg")],
+    // Specify symbolId format
+    symbolId: "icon-[dir]-[name]",
+    svgoOptions: isBuild
+  });
 }
